e5b61edaf3da7457daa464aa848237029d6cc273
vitaly.v <volkov.vitaly.v@gmail.com>
2019-02-10 22:21:27 +0300

    optimized code: removed string swapping and reversing

--- sum.php
+++ sum.php
@@ -10,47 +10,36 @@
  */
 function bigSum(string $num1, string $num2): string
 {
-    $length1 = strlen($num1);
-    $length2 = strlen($num2);
+    $minLength = $length1 = strlen($num1);
+    $maxLength = $length2 = strlen($num2);
+    $isSecondLarger = true;
 
-    if ($length1 > $length2) {
-        $t = $num1;
-        $num1 = $num2;
-        $num2 = $t;
-
-        $t = $length1;
-        $length1 = $length2;
-        $length2 = $t;
+    if ($length1 > $length2) { // make sure length of second number is larger
+        $minLength = $length2;
+        $maxLength = $length1;
+        $isSecondLarger = false;
     }
-
-    $num1 = strrev($num1);
-    $num2 = strrev($num2);
 
     $zero = 48; // ord('0')
     $result = '';
     $carry = 0;
-    for ($i = 0; $i < $length1; $i++) {
-        $n1 = ord($num1[$i]) - $zero;
-        $n2 = ord($num2[$i]) - $zero;
+
+    for ($i = 0; $i < $maxLength; $i++) { // calc sum digit by digit from right to left
+        $n1 = 0; // if there are no digits in smaller number just add remaining digits of larger number
+        if ($i < $minLength) { // both numbers have digits in this position
+            $n1 = ord($isSecondLarger ? $num1[$length1 - $i - 1] : $num2[$length2 - $i - 1]) - $zero;
+        }
+        $n2 = ord($isSecondLarger ? $num2[$length2 - $i - 1] : $num1[$length1 - $i - 1]) - $zero;
+
         $sum = $n1 + $n2 + $carry;
 
-        $result .= chr($sum % 10 + $zero);
-        $carry = (int)($sum / 10);
-    }
-
-    for ($i = $length1; $i < $length2; $i++) {
-        $n = ord($num2[$i]) - $zero;
-        $sum = $n + $carry;
-
-        $result .= chr($sum % 10 + $zero);
+        $result = chr($sum % 10 + $zero) . $result;
         $carry = (int)($sum / 10);
     }
 
     if ($carry) {
-        $result .= chr($carry + $zero);
+        $result = chr($carry + $zero) . $result;
     }
-
-    $result = strrev($result);
 
     return $result;
 }
