56c706d3858aaec459e08eee163e9f0ea24bdf64
vitaly.v <volkov.vitaly.v@gmail.com>
2019-02-10 21:48:22 +0300

    added benchmarks bases on bcadd, added random examples

--- run.php
+++ run.php
@@ -5,62 +5,78 @@ require_once __DIR__ . '/sum.php';
 /**
  * @param string $num1
  * @param string $num2
- * @param string $expectedSum
  */
-function runExample(string $num1, string $num2, string $expectedSum)
+function runExample(string $num1, string $num2)
 {
+    $expectedSum = bcadd($num1, $num2);
     $actualSum = bigSum($num1, $num2);
 
     $isEquals = $actualSum === $expectedSum;
 
-    $maxLen = max(strlen($num1), strlen($num2), strlen($actualSum)) + 11;
+    $leftPadding = 10;
+    $maxLen = max(strlen($num1), strlen($num2), strlen($actualSum));
     $equals = $isEquals ? "\033[0;32m✔\033[0m" : "\033[0;31m✗\033[0m";
 
-    echo "\nTest\n";
-    printf("%{$maxLen}s\n", $num1);
-    printf("%{$maxLen}s\n", $num2);
-    printf("%{$maxLen}s\n", "= $actualSum");
-    printf("%{$maxLen}s\n", "Expected $equals $expectedSum");
+    printf("Test\n");
+    printf("%{$leftPadding}s %{$maxLen}s\n", '', $num1);
+    printf("%{$leftPadding}s %{$maxLen}s\n", '+', $num2);
+    printf("%{$leftPadding}s %{$maxLen}s\n", '=', $actualSum);
+    printf("%{$leftPadding}s %{$maxLen}s\n", "Expected $equals", $expectedSum);
+}
+
+function randomStr($len) {
+    $al = str_split('0123456789');
+    $str = '';
+    for($i = 0; $i < $len; $i++) {
+        do {
+            $ch = $al[array_rand($al)];
+        } while ($i === 0 && $ch === '0');
+
+        $str .= $ch;
+    }
+
+    return $str;
 }
 
 runExample(
     '0',
-    '0',
-  '0'
+    '0'
 );
 
 runExample(
   '2',
-  '3',
-  '5'
+  '3'
 );
 
 runExample(
   '1234567890',
-  '3',
-  '1234567893'
+  '3'
 );
 
 runExample(
   '3',
-  '1234567890',
-  '1234567893'
+  '1234567890'
 );
 
 runExample(
   '123456789',
-  '99',
-  '123456888'
+  '99'
 );
 
 runExample(
   '99',
-  '123456789',
-  '123456888'
+  '123456789'
 );
 
 runExample(
   '9000000000000000001',
-  '9000000000000000002',
-  '18000000000000000003'
+  '9000000000000000002'
 );
+
+// 10 random examples
+for ($i = 0; $i < 10; $i++) {
+    $num1 = randomStr(random_int(1, 100));
+    $num2 = randomStr(random_int(1, 100));
+
+    runExample($num1, $num2);
+}
